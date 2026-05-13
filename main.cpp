9f21faf4b5ce43bf67f1b9519bc67cc55d99ea58
madhumani2710-boop <madhumani2710@gmail.com>
2026-05-13 11:48:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,13 @@
 unsigned long lastSample = 0;
 
 void setup() {
-    // Standard ESP32 Setup
     Serial.begin(BAUD_RATE);
-    
-    // Wait for Serial to initialize
+
     while(!Serial && millis() < 3000); 
     
-    analogReadResolution(12); // Sets ADC to 0-4095
-    analogSetAttenuation(ADC_11db); // Allows range up to ~3.3V
+    analogReadResolution(12);
+    analogSetAttenuation(ADC_11db); 
     
-    // This string MUST match what final.py is looking for
     Serial.println("AUTISM_TOY_MIC_READY");
 }
 
@@ -33,8 +30,7 @@ void loop() {
         }
         
         int average = (int)(sum / ADC_SAMPLES);
-        
-        // Print the raw value for the Python backend to process
+      
         Serial.println(average);
     }
 }
